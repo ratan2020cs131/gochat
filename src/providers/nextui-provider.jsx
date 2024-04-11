2e4e909bf70cf25533e1abfa8b80d6d6ddeed2b7
Ratan Deep Singh <91245947+ratan2020cs131@users.noreply.github.com>
2024-04-12 00:45:35 +0530

    next ui setted up

---
+++ src/providers/nextui-provider.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+
+export default ({ children }) => {
+    return (
+        <NextUIProvider>
+            {children}
+        </NextUIProvider>
+    );
+}
